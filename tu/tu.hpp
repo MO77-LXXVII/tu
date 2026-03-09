746b2fb90a26145be21bc39191bbe5189f155c40
MO77-LXXVII <mohanad20022007@gmail.com>
2026-03-09 14:43:26 +0200

    chores: add `tu.hpp` umbrella header for terminal utils

---
+++ tu/tu.hpp
@@ -0,0 +1,13 @@
+// tu.hpp
+
+#pragma once
+
+// umbrella header: includes all terminal_utils (tu) headers
+
+#include "tu/ansi_colors.hpp"
+#include "tu/style_wrappers.hpp"
+#include "tu/input.hpp"
+#include "tu/output.hpp"
+#include "tu/logger.hpp"
+#include "tu/menu/menu.hpp"
+#include "tu/menu/menu_navigator.hpp"
