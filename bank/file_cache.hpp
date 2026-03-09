c878f984316e95952d4192a9de504cd1f8fd172a
MO77-LXXVII <mohanad20022007@gmail.com>
2026-03-09 12:25:08 +0200

    feat(FileCache): Add `FileCache` base template class to be used for caching

---
+++ bank/file_cache.hpp
@@ -0,0 +1,23 @@
+// file_cache.hpp
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <optional>
+#include <string_view>
+
+
+
+
+template<typename Derived>
+class FileCache
+{
+    public:
+
+
+
+
+
+};
