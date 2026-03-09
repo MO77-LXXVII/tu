d5478912e84bcdd57341f48c378ce050eabae916
MO77-LXXVII <mohanad20022007@gmail.com>
2026-03-09 12:33:07 +0200

    feat(FileCache): Add `load()`, `m_populate()` and `s_cache`

--- bank/file_cache.hpp
+++ bank/file_cache.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <optional>
@@ -16,8 +17,35 @@ class FileCache
 {
     public:
 
+        [[nodiscard]] static std::vector<Derived> load()
+        {
+            if (s_cache.has_value())
+                return *s_cache;
+
+            return m_populate();
+        }
 
 
+    private:
+        // will carry Derived file data
+        inline static std::optional<std::vector<Derived>> s_cache;
 
 
+        static std::vector<Derived>& m_populate()
+        {
+            std::vector<Derived> records;
+
+            std::ifstream file(Derived::file_name().data());
+
+            if(file.is_open())
+            {
+                std::string line;
+                while(std::getline(file, line))
+                    if(!line.empty())
+                        records.push_back(Derived::decode(line));
+            }
+
+            s_cache = std::move(records);
+            return *s_cache;
+        }
 };
