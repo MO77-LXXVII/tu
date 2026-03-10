af3a94e4c05b320391396c9545edc9219fb581f5
MO77-LXXVII <mohanad20022007@gmail.com>
2026-03-10 15:58:54 +0200

    chores: run `run_bank_demo()` by default

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main()
     // alternate implementation that demonstrates that `Menu` works standalone
     // with any custom navigation structure; uses a raw `std::stack<std::string>`
     // to manually track and switch menus
-    run_bank_demo_raw();
+    // run_bank_demo_raw();
 
     // production entry point; uses `MenuNavigator` which wraps the `std::stack`
     // and abstracts away the manual navigation logic for the menus
-    // run_bank_demo();
+    run_bank_demo();
 
     return 0;
 }
